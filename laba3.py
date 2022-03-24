12fa57bd8ad76eac5330313fbd3edef941d0d3cc
Varymas <101781663+Varymas@users.noreply.github.com>
2022-03-24 09:50:25 +0600

    Add files via upload

---
+++ laba3.py
@@ -0,0 +1,16 @@
+import math
+a=int(input("a="))
+b=int(input("b="))
+c=int(input("c="))
+if a<=3 and a>=1:
+    print("a=true")
+else:
+    print("a=false")
+if b<=3 and b>=1:
+    print("b=true")
+else:
+    print("b=false")
+if c<=3 and c>=1:
+    print("c=true")
+else:
+    print("c=false")
